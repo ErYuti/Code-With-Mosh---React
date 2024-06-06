003f7a32adf25ed4ba038054c9007a5e9ecdf15e
Er.Yuti <yutimeher@gmail.com>
2024-06-06 13:20:31 +0530

    file added

---
+++ 1_Getting Started/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+//react is module , React is the object that importing from react 
+import ReactDOM from 'react-dom';
+
+
+const element = <h1>Hello world</h1>;
+console.log(element);
+
+ReactDOM.render(element, document.getElementById('root'));
